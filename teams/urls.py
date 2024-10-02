7f9a56dda0a6a1b495953f983af78145167874ac
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-10-02 22:23:59 +0300

    add base urls for teams, tasks

---
+++ teams/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from teams.views import TeamViewSet, TaskViewSet
+
+app_name = "teams"
+
+router = routers.DefaultRouter()
+router.register("teams", TeamViewSet)
+router.register("tasks", TaskViewSet)
+
+urlpatterns = [path("", include(router.urls))]
