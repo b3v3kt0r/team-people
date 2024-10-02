600841747068356a2ec70eb513aff93a5582ac46
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-10-02 22:17:25 +0300

    add base models for teams

--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,20 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    date_of_establishment = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    assigned_to = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="tasks")
+    teams = models.ManyToManyField(Team, related_name="tasks")
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title}. Responsible: {self.assigned_to.name}. Completed: {self.completed}"
