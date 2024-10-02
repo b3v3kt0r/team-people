d8783242869a2d2c0530e5a62a927bc272f6b14a
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-10-02 23:43:31 +0300

    fix user models

--- user/models.py
+++ user/models.py
@@ -44,7 +44,7 @@ class User(AbstractUser):
 
     username = None
     email = models.EmailField(_("email address"), unique=True)
-    team = models.ForeignKey(Team, related_name="users", on_delete=models.CASCADE, default="no team")
+    team = models.ForeignKey(Team, related_name="users", on_delete=models.CASCADE, null=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
