57ac53f1cd0efe8b035ba687eefcc28f9a3fbb9e
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-10-03 10:27:33 +0300

    fix models in teams

--- teams/models.py
+++ teams/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Team(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     date_of_establishment = models.DateField()
 
     def __str__(self):
