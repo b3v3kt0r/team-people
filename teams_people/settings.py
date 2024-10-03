1197bb3f9f06582b8fa947170d25c10bf0159a35
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-10-03 12:12:31 +0300

    update jwt settings

--- teams_people/settings.py
+++ teams_people/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -138,3 +138,9 @@ INTERNAL_IPS = [
     "127.0.0.1",
     # ...
 ]
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
+}
